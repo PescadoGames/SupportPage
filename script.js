6c78791450422ee832815a425c869f7b65f0b815
2315195882 <69532542+2315195882@users.noreply.github.com>
2021-07-31 04:04:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-$(function(){
-  $('.btn-trigger').on('click', function() {
-    $(this).toggleClass('active');
-    return false;
-  });
-});
-
 document.getElementById('hamburger').addEventListener('click',  function(){
         $('.icon').toggleClass('close');
         $('.sm').slideToggle();
