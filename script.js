1c0291cc07d03f1dedb83bccaa4f5375e574cd2a
2315195882 <69532542+2315195882@users.noreply.github.com>
2021-07-31 05:22:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-
+    time();
+    function time(){
+        var now = new Date();
+        document.getElementById("time").innerHTML = now.toLocaleTimeString();
+    }
+    setInterval('time()',1000);
 
 
 
