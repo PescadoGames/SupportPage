f2bd2f007d1884ebbf00cdd134fa9c2dabacad2e
2315195882 <69532542+2315195882@users.noreply.github.com>
2021-07-31 03:13:01 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function(){
+  $('.btn-trigger').on('click', function() {
+    $(this).toggleClass('active');
+    return false;
+  });
+});
+
+document.getElementById('hamburger').addEventListener('click',  function(){
+        $('.icon').toggleClass('close');
+        $('.sm').slideToggle();
+        })
+
+
+
+
+
+
+
+
+
+
+
