f3dacc71ec97f2d09ca250663e2c6a6df0912cf7
2315195882 <69532542+2315195882@users.noreply.github.com>
2021-09-14 17:42:12 +0900

    一回転用のjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-
+function turn(){
+    // bodyのクラス名を取得
+    var ori = document.body.className;
+    // bodyのクラスに追加
+    document.body.className = ori + " turn"; 
+ 
+    // 一回転後、クラスを元の状態にする。
+    setTimeout(function(){
+        document.body.className = ori;
+    },2000);
+}
