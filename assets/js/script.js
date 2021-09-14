6c5c7051d52e2e0dc46802775741e23248cb9d04
2315195882 <69532542+2315195882@users.noreply.github.com>
2021-09-14 17:51:35 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,15 @@ function turn(){
         document.body.className = ori;
     },2000);
 }
+
+function turn10(){
+    // bodyのクラス名を取得
+    var ori = document.body.className;
+    // bodyのクラスに追加
+    document.body.className = ori + " turn10"; 
+ 
+    // 一回転後、クラスを元の状態にする。
+    setTimeout(function(){
+        document.body.className = ori;
+    },2000);
+}
