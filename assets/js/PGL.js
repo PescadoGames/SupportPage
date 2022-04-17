450636956badd67bc487d1731dc717b75917bdbe
2315195882 <69532542+2315195882@users.noreply.github.com>
2022-04-17 13:59:42 +0900

    Update PGL.js

--- assets/js/PGL.js
+++ assets/js/PGL.js
@@ -23,3 +23,61 @@
 		}
 	]
 	});
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 100){//上から200pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 1500,"easeInOutQuint");//ページトップスクロールの速さ※数字が大きいほど遅くなる, easingプラグインでアニメーション速度に変化
+	//linear、swing、jswing、easeInQuad、easeOutQuad、easeInOutQuad、easeInCubic、easeOutCubic、easeInOutCubic、easeInQuart、easeOutQuart、easeInOutQuart、easeInQuint、easeOutQuint、easeInOutQuint、easeInSine、easeOutSine、easeInOutSine、easeInExpo、easeOutExpo、easeInOutExpo、easeInCirc、easeOutCirc、easeInOutCirc、easeInElastic、easeOutElastic、easeInOutElastic、easeInBack、easeOutBack、easeInOutBack、easeInBounce、easeOutBounce、easeInOutBounceなどから選択可能
+    return false;//リンク自体の無効化
+});
+
+//スクロールした際の動きを関数でまとめる
+function HeaderAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 100){//上から200pxスクロールしたら
+		$('header').removeClass('DownMove');//headerについているDownMoveというクラス名を除く
+		$('header').addClass('UpMove');//headerについているUpMoveというクラス名を付与
+	}else{
+		if($('header').hasClass('UpMove')){//すでにheaderにUpMoveというクラス名がついていたら
+			$('header').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('header').addClass('DownMove');//DownMoveというクラス名をheaderに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	HeaderAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	HeaderAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
