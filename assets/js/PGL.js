90fe2b6cc71ba20d3ff5993accb59462f9db8f3a
2315195882 <69532542+2315195882@users.noreply.github.com>
2022-04-20 04:47:36 +0900

    Update PGL.js

--- assets/js/PGL.js
+++ assets/js/PGL.js
@@ -31,27 +31,3 @@ $('#page-top').click(function () {
 	//linear、swing、jswing、easeInQuad、easeOutQuad、easeInOutQuad、easeInCubic、easeOutCubic、easeInOutCubic、easeInQuart、easeOutQuart、easeInOutQuart、easeInQuint、easeOutQuint、easeInOutQuint、easeInSine、easeOutSine、easeInOutSine、easeInExpo、easeOutExpo、easeInOutExpo、easeInCirc、easeOutCirc、easeInOutCirc、easeInElastic、easeOutElastic、easeInOutElastic、easeInBack、easeOutBack、easeInOutBack、easeInBounce、easeOutBounce、easeInOutBounceなどから選択可能
     return false;//リンク自体の無効化
 });
-
-//スクロールした際の動きを関数でまとめる
-function HeaderAnime() {
-	var scroll = $(window).scrollTop();
-	if (scroll >= 100){//上から200pxスクロールしたら
-		$('header').removeClass('UpMove');//#page-topについているDownMoveというクラス名を除く
-		$('header').addClass('DownMove');//#page-topについているUpMoveというクラス名を付与
-	}else{
-		if($('header').hasClass('DownMove')){//すでに#page-topにUpMoveというクラス名がついていたら
-			$('header').removeClass('DownMove');//UpMoveというクラス名を除き
-			$('header').addClass('UpMove');//DownMoveというクラス名を#page-topに付与
-		}
-	}
-}
-
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-	HeaderAnime();/* スクロールした際の動きの関数を呼ぶ*/
-});
-
-// ページが読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function () {
-	HeaderAnime();/* スクロールした際の動きの関数を呼ぶ*/
-});
