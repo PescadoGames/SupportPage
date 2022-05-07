7070e80865f6bad72b1746b729af7e0721f47164
2315195882 <69532542+2315195882@users.noreply.github.com>
2022-05-07 09:34:06 +0900

    Update Download.js

--- assets/js/Download.js
+++ assets/js/Download.js
@@ -1,17 +1,3 @@
-function DownloadURLChecker() {
-    if (location.search == '?PescadoGamesLauncher') {
-    // PescadoGamesLauncher
-    $("#link_text a").attr("href", "https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg");
-    $("#ThankYouDownloadMessage").text("<h2>PescadoGamesLauncherをダウンロードしていただきありがとうございます！</h2>");
-  } else {
-    $("#ThankYouDownloadMessage").text("<h2>このファイルはありません<br>URLが間違っていないかもう一度お確かめください</h2>");
-  }
-}
-
-$(window).on('load', function () {
-    DownloadURLChecker();
-});
-
 window.onload = function onLoad() {
   doDownload('https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg', 'PescadoGamesLauncherInstaller.exe');
   target = document.getElementById("ThankYouDownloadMessage");
