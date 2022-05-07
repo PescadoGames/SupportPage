d91d6d32a4c622a5f6cd3a92a5caa7669d866618
2315195882 <69532542+2315195882@users.noreply.github.com>
2022-05-07 11:54:54 +0900

    Update EnDownload.js

--- assets/js/EnDownload.js
+++ assets/js/EnDownload.js
@@ -1,8 +1,8 @@
 window.onload = function onLoad() {
   if (location.search == "?PescadoGamesLauncher") {
-      document.location.assign("https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg");
+      document.location.assign("https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21471&authkey=AK7UCEVz0DUFrME");
       document.getElementById('ThankYouDownloadMessage').innerHTML = "<h2>Thank you for downloading PescadoGamesLauncher!</h2>";
-      document.getElementById('DownloadHref').innerHTML = '<a href="https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg">If the download does not start, click here.</a>';
+      document.getElementById('DownloadHref').innerHTML = '<a href="https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21471&authkey=AK7UCEVz0DUFrME">If the download does not start, click here.</a>';
   }
   else{
     document.getElementById('ThankYouDownloadMessage').innerHTML = "<h2>This file is not available<br>Please check the URL and try again</h2>";
