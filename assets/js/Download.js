dde1da866f15ef63b1b65574d2fb677f31e59e4a
2315195882 <69532542+2315195882@users.noreply.github.com>
2022-05-07 09:33:45 +0900

    Update Download.js

--- assets/js/Download.js
+++ assets/js/Download.js
@@ -11,3 +11,15 @@ function DownloadURLChecker() {
 $(window).on('load', function () {
     DownloadURLChecker();
 });
+
+window.onload = function onLoad() {
+  doDownload('https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg', 'PescadoGamesLauncherInstaller.exe');
+  target = document.getElementById("ThankYouDownloadMessage");
+  target.innerHTML = "<h2>PescadoGamesLauncherをダウンロードしていただきありがとうございます！</h2>";
+  const target = document.getElementById("DownloadHref")
+
+  const changeHref = () => {
+    target.href = "https://www.yahoo.co.jp"
+    target.textContent = "Yahoo"
+  }
+}
