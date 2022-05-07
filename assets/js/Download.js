e6eea32341a7b27458d9b9cc93f5820873c9d778
2315195882 <69532542+2315195882@users.noreply.github.com>
2022-05-07 10:18:24 +0900

    Update Download.js

--- assets/js/Download.js
+++ assets/js/Download.js
@@ -1,5 +1,11 @@
 window.onload = function onLoad() {
-  document.location.assign("https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg");
-  document.getElementById('ThankYouDownloadMessage').innerHTML = "<h2>PescadoGamesLauncherをダウンロードしていただきありがとうございます！</h2>";
-  document.getElementById('DownloadHref').innerHTML = '<a href="https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg">ダウンロードが始まらない場合はこちら</a>';
+  if (location.search == "?PescadoGamesLauncher") {
+      document.location.assign("https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg");
+      document.getElementById('ThankYouDownloadMessage').innerHTML = "<h2>PescadoGamesLauncherをダウンロードしていただきありがとうございます！</h2>";
+      document.getElementById('DownloadHref').innerHTML = '<a href="https://onedrive.live.com/download?cid=A4B976D6D52E89DF&resid=A4B976D6D52E89DF%21470&authkey=APNEcWa-c7AulLg">ダウンロードが始まらない場合はこちら</a>';
+  }
+  else{
+    document.getElementById('ThankYouDownloadMessage').innerHTML = "<h2>このファイルはありません<br>URLを確認してもう一度お試しください</h2>";
+  }
+
 }
